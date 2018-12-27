8bfb554506f738fce7a40e34c9431fe9d1217b2a
Arie Milner <amilner42@gmail.com>
2018-12-27 14:53:24 -0800

    Remove backend - Upgrading from scratch from external repo: https://github.com/gothinkster/node-express-realworld-example-app

--- backend/typings_manual/augmented_modules/passport.d.ts
+++
@@ -1,7 +0,0 @@
-export {};
-
-declare module "passport" {
-
-  function deserializeUser(fn: (id: any, done: (err: any, user?: any, mssg?: Object) => void) => void): void;
-  function authenticate(strategy: string, callback: Function): Function;
-}
